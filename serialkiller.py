ba05f24def7734c21baad63697b69938bbb7888b
GirijaManoj kumar reddy Kalakoti <kgmkr00001@gmail.com>
2018-03-02 19:05:57 +0530

    Update serialkiller.py

--- serialkiller.py
+++ serialkiller.py
@@ -1,3 +1,8 @@
+'''There are N people standing in a circle. A serial killer who wants to kill every K'th person in the circle except the last one
+in the circle. Joseph who is very good at math want to survive in this process but wants your help to survive,Help him by finding 
+the position that he have to stand'''
+
+
 n=int(input())
 k=int(input())
 a=[]
