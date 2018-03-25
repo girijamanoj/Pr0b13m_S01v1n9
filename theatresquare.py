53235a8f1202c0651e4aae0275f0b6b70279e70a
Gopa Vasanth <gopavasanth1999@gmail.com>
2018-03-25 20:21:12 +0530

    Update theatresquare.py

--- theatresquare.py
+++ theatresquare.py
@@ -1,10 +1,10 @@
 k=list(map(int,input().split(" ")))
-if k[0]%k[2]==0:
+if (k[0]%k[2]==0):
     p=k[0]//k[2]
-elif k[0]%k[2]!=0:
+elif (k[0]%k[2]!=0):
     p=(k[0]//k[2])+1
-if k[1]%k[2]==0:
+if (k[1]%k[2]==0):
     q=k[1]//k[2]
-elif k[1]%k[2]!=0:
+elif (k[1]%k[2]!=0):
     q=(k[1]//k[2])+1    
 print(p*q)
