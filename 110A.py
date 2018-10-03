ad2d24e1fc74bb824382211374caa65d0537ddfe
ShivakumarShoban <43823172+ShivakumarShoban@users.noreply.github.com>
2018-10-03 22:19:54 +0530

    Update 110A.py

--- 110A.py
+++ 110A.py
@@ -1,4 +1,4 @@
-p=int(input())
+p=int(input()) #taking input and typecasting it
 k=str(p)
 if str(k.count('7')+k.count('4')).count('7')+str(k.count('7')+k.count('4')).count('4')==len(str(k.count('7')+k.count('4'))):
     print("YES")
