ef79e2830506174728e6de22ca380b49b6ceb26e
GirijaManoj <kgmkr00001@gmail.com>
2019-02-12 09:22:57 +0000

    Delete 10A.py

--- 10A.py
+++
@@ -1,8 +0,0 @@
-"""Usage of map and split"""
-l=list(map(int,(input().split())))
-for i in range(l[0]):
-    k=list(map(int,(input().split())))
-    
-    s=s+(k[1]-k[0])*l[1] """ is this in loop or not? intend properly!""" 
-    if i>0:
-        
